8968618989dc64c062497eb01ad7255e4351369e
austin1965 <83925251+austin1965@users.noreply.github.com>
2023-12-01 23:13:14 -0600

    Set up failing test for implementation of unscrambling logic.

---
+++ src/main/java/CalibrationUnscrambler.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class CalibrationUnscrambler {
+    public int unscramble(File scrambledCalibrationInfo) {
+        return 0;
+    }
+}
---
+++ src/test/java/CalibrationValueTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalibrationValueTest {
+
+    @Test
+    @Disabled("WIP")
+    void shouldFindCalibrationValue() {
+        //given
+        CalibrationUnscrambler systemUnderTest = new CalibrationUnscrambler();
+        File scrambledCalibrationInfo = new File("src/test/resources/example.txt");
+
+        //when
+        int calibrationValue = systemUnderTest.unscramble(scrambledCalibrationInfo);
+
+        //then
+        assertEquals(calibrationValue, 77);
+
+    }
+
+}
