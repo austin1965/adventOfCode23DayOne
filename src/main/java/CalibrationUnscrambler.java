1505ba4bdd43f499c452495dba14ed33712593d3
austin1965 <83925251+austin1965@users.noreply.github.com>
2023-12-02 12:34:03 -0600

    Add extraction of first and list integer. Add Summing of values

--- src/main/java/CalibrationUnscrambler.java
+++ src/main/java/CalibrationUnscrambler.java
@@ -4,25 +4,42 @@ import java.util.List;
 
 public class CalibrationUnscrambler {
     public int unscramble(File scrambledCalibrationInfo) {
-
-        List<List<Integer>> calibrationDataTable = extractCalibrationData(scrambledCalibrationInfo);
-        System.out.println(calibrationDataTable);
-
-        return 142;
+        List<List<String>> calibrationDataTable = extractCalibrationData(scrambledCalibrationInfo);
+        List<String> valuesForCalculation = determineCalibrationValues(calibrationDataTable);
+        return getCalibrationValue(valuesForCalculation);
     }
 
-    private static List<List<Integer>> extractCalibrationData(File scrambledCalibrationInfo) {
-        List<List<Integer>> calibrationDataTable = new ArrayList<>();
+    private static int getCalibrationValue(List<String> valuesForCalculation) {
+        return valuesForCalculation.stream().map(Integer::parseInt).reduce(0, Integer::sum);
+    }
+
+    private static List<String> determineCalibrationValues(List<List<String>> calibrationDataTable) {
+
+        return calibrationDataTable.stream().map(list -> {
+            String  result;
+            if (list.size() == 1) {
+                result = list.get(0) + list.get(0);
+            }
+            else {
+                result = list.get(0) + list.get(list.size() - 1);
+            }
+            return result;
+        }).toList();
+    }
+
+    private static List<List<String>> extractCalibrationData(File scrambledCalibrationInfo) {
+        List<List<String>> calibrationDataTable = new ArrayList<>();
 
         try (BufferedReader br =  new BufferedReader(new FileReader(scrambledCalibrationInfo))) {
-            List<Integer> calibrationDataRow = new ArrayList<>();
+            List<String> calibrationDataRow = new ArrayList<>();
 
             int rawDatum = br.read();
             while(rawDatum != -1) {
                 char datum = (char) rawDatum;
 
                 if (Character.isDigit(datum)) {
-                    calibrationDataRow.add(Character.getNumericValue(datum));
+                    int temp = Character.getNumericValue(datum);
+                    calibrationDataRow.add(String.valueOf(temp));
                 }
                 else if (datum == '\n') {
                     calibrationDataTable.add(calibrationDataRow);
